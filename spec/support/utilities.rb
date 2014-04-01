22213f7ab7e6d6159a9373b74a4ae1d041063ab9
CJ Zara <chesterjzara@gmail.com>
2014-04-01 16:26:12 -0500

    Finish 8.5 Exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,15 +1,20 @@
 include ApplicationHelper
 
-#new stuff
-
 def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
 
-RSpec::Matchers.define :have_error_message do |message|
+def valid_signup()
+    fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar"
+    fill_in "Confirmation", with: "foobar"
+end
+
+RSpec::Matchers.define :have_message do |message|
   match do |page|
-    expect(page).to have_selector('div.alert.alert-error', text: message)
+    expect(page).to have_selector('div.alert', text: message)
   end
 end
